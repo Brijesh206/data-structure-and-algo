2d3309f784a7c4dc8de749aec67d34d4867c80ce
Brijesh <brijeshbhesaniya12@gmail.com>
2022-07-17 09:33:04 +0530

    Program to rotate a matrix by 90 degree without using extra space. ( gfg question )

---
+++ src/com/company/RotateBy90Degree.java
@@ -0,0 +1,67 @@
+package com.company;
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class RotateBy90Degree
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
+
+        while(t-- > 0)
+        {
+            int n = Integer.parseInt(read.readLine());
+            int matrix[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(st[k++]);
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix, n);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][], int n)
+    {
+        // code
+        int arr[][];
+        arr = matrix;
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i; j < arr[0].length; j++) {
+                int temp = arr[j][i];
+                arr[j][i] = arr[i][j];
+                arr[i][j] = temp;
+            }
+
+        for (int i = 0; i < arr[0].length; i++)
+            for (int j = 0, k = arr[0].length - 1; j < k;
+                 j++, k--) {
+                int temp = arr[j][i];
+                arr[j][i] = arr[k][i];
+                arr[k][i] = temp;
+            }
+    }
+}
